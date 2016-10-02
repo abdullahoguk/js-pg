6ab2bd65c9d41b4585be4e878fd5e8c672a36b57
Abdullah Öğük <abdullahoguk@gmail.com>
2016-10-02 22:18:49 +0300

    2

---
+++ todo-reactNmeteor/client/main.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+
+import { Meteor } from 'meteor/meteor';
+
+import { render } from 'react-dom';
+
+
+
+import App from '../imports/ui/App.jsx';
+
+
+
+Meteor.startup(() => {
+
+  render(<App />, document.getElementById('render-target'));
+
+});
+
+
+//MyCollection = new Mongo.Collection("my-collection");
